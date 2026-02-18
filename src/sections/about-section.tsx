a5708a7fd1d4ca8830633415cfb5d1a9bc3bf0d9
Harsh7-dit <harshmasane43@gmail.com>
2026-02-17 21:34:50 -0500

    Updated Content 1

--- src/sections/about-section.tsx
+++ src/sections/about-section.tsx
@@ -5,10 +5,10 @@ export default function AboutSection() {
         <Section title="About">
             <div className="text-sm/6.5">
                 <p>
-                    I’m Rayan Walker, a full-stack developer passionate about building end-to-end digital experiences that are fast, scalable and user-focused. With strong expertise in both front-end and back-end technologies, I enjoy transforming ideas into fully functional web applications that feel seamless and intuitive.
+                    I'm Harsh Masane, a DevOps Engineer and Cloud Enthusiast passionate on automating CI/CD pipelines, improving infrastructure reliability, and optimizing system performance, database and storage optimization. My experience intersect with automation,  containerization, orchestration , infrastructure as code with monitoring and observability.
                 </p>
                 <p className="mt-5">
-                    Whether it’s crafting responsive interfaces or developing robust backend systems, I aim to deliver solutions that are both reliable and impactful.
+                    I love solving issues - especially at the system level and then automating the solution so it never happens again. It’s deeply satisfying to make a service faster, safer, and more reliable not just once, but permanently.
                 </p>
             </div>
         </Section>
