204e05757b089ec151e7ef8b2148653793978832
Harsh7-dit <harshmasane43@gmail.com>
2026-02-20 18:32:23 -0500

    Added the project section1

--- src/sections/projects-section.tsx
+++ src/sections/projects-section.tsx
@@ -3,38 +3,46 @@ import Section from "../components/section";
 export default function ProjectsSection() {
     const projects = [
         {
-            title: "Marketplace",
-            description: "E-commerce web app",
-            image: "/assets/project-1.png",
+            title: "Live Chat Application",
+            description: "A Flask application with live chat functionality ",
+            link: "https://github.com/Harsh7-dit/Live_Chat_Application_2_Tier_Over_Kubernetes_Cluster_GCP",
+            image: "assets/Live_Chat.png",
         },
         {
-            title: "Navigator",
-            description: "Navigation app design",
-            image: "/assets/project-2.png",
+            title: "Self-Healing Backup Monitoring System",
+            description: "Automatically detects and resolves backup failures using a self-healing mechanism.",
+            link: "https://github.com/Harsh7-dit/Self-Healing_MongoDB_Backup_Monitor",
+            image: "assets/Live_Chat.png",
         },
     ];
 
     return (
         <Section title="Projects">
             <div className="flex flex-wrap justify-center items-center gap-4 mr-auto">
-                {projects.map((project) => (
-                    <div key={project.title} className="hover:-translate-y-0.5 transition duration-300 max-w-75 border border-gray-200 rounded-xl">
-                        <img
-                            className="rounded-t-xl h-42 object-fit"
-                            src={project.image}
-                            alt={project.title}
-                            width={300}
-                            height={170}
-                        />
-                        <div className="p-4">
-                            <h3 className="text-base font-medium">
-                                {project.title}
-                            </h3>
-                            <p className="text-gray-500 mt-1">
-                                {project.description}
-                            </p>
-                        </div>
+            {projects.map((project) => (
+                <a
+                    key={project.title}
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:-translate-y-0.5 transition duration-300 max-w-75 border border-gray-200 rounded-xl block cursor-pointer"
+                >
+                    <img
+                    className="rounded-t-xl h-42 object-cover"
+                    src={project.image}
+                    alt={project.title}
+                    width={300}
+                    height={170}
+                    />
+                    <div className="p-4">
+                    <h3 className="text-base font-medium">
+                        {project.title}
+                    </h3>
+                    <p className="text-gray-500 mt-1">
+                        {project.description}
+                    </p>
                     </div>
+                </a>
                 ))}
             </div>
         </Section>
